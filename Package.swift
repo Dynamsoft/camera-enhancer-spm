9c0b30e8d818ab304c0c6cc111d7944dd148262c
arckongem <arckongem@gmail.com>
2022-09-23 16:28:29 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DynamsoftCameraEnhancer",
-            url: "https://download.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-2.3.3-xcframework.zip",
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-2.3.3-xcframework.zip",
             checksum: "72084c2bc4d1710ff37fd32f0e4f220e5fe42bc8bb69bd90fe409db511ce2060"
         ),
     ]
