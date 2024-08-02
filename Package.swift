11e464d31adc8cdd871bcdcab673e762beaca856
cjpyxp <40759083+cjpyxp@users.noreply.github.com>
2024-08-02 16:54:12 +0800

    update to 2.3.22

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DynamsoftCameraEnhancer",
-            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-2.3.21-xcframework.zip",
-            checksum: "d25e613187ff46e2c7b7e303a9e441bd1728dc48aeb19450214e009a59a15eea"
+            url: "https://download2.dynamsoft.com/xcframeworks/dynamsoft-cameraenhancer-ios-2.3.22-xcframework.zip",
+            checksum: "81cac6c96d7cc9d4e89015f5cc6130e27f24dfbb50d9454d8130eb332d587269"
         ),
     ]
 )
